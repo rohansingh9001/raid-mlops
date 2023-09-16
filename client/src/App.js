9c943807c2a17f8b243df6a8e955535f352c257e
rohansingh-chat360 <rohan.singh@chat360.io>
2023-09-16 10:57:00 +0530

    Added react page

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './components/QueryForm'
+import QueryForm from './components/QueryForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>News Headline Classifier</h1>
+      <QueryForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/QueryForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+function QueryForm() {
+  const [query, setQuery] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await fetch('http://34.87.37.253/predict', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const data = await response.json();
+      setResponse(data.prediction);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className='textbox'
+          type="text"
+          placeholder="Enter your query"
+          value={query}
+          onChange={handleInputChange}
+        />
+        <button className='submitButton' type="submit">Submit</button>
+      </form>
+      {response && (
+        <div>
+          <h2>Response:</h2>
+          <p>{response}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default QueryForm;
